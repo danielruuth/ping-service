7357f584652c9c8729393e0c75f2f49bc878e140
Daniel Ruuth <danielruuth@gmail.com>
2016-02-24 11:41:24 +0100

    Moved to socket
    Removed usage of db in favour for socket for real time monitoring via
    browser.

--- app.js
+++ app.js
@@ -10,85 +10,62 @@ var smtp = {
 	"password": ""
 }
 
-var hosts = [
-	{"host":'domain.com', "status":true, "contact":"email@domain.com"}
-];
+//Load settings from external file
+var hosts = require( __dirname + '/hosts.json');
 
 
 //Packages
 var ping = require('ping');
-var express = require('express');
-var mongo = require('mongodb');
 var nodemailer = require('nodemailer');
-
-
-
-
-
-//Packages
-var ping = require('ping');
 var express = require('express');
-var mongo = require('mongodb');
-
-
-
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
-
-var dBserver = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('pingback_service', dBserver);
-
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'hoyservice_collections' database");
-    }else{
-    	console.log('Mongo error');
-    }
-});
+var nodemailer = require('nodemailer');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-var nodemailer = require('nodemailer');
 
+//Setup express
+app.use(express.static('static'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+
+
+var lastupdated = new Date();
+var incident_time;
+
+
+
+//Setup socket
+io.on('connection', function(socket){
+	socket.emit('data', {hosts:hosts});
+});
 
 
 //Setup mail
 if( smtp.host !== "" ){
 	var transporter = nodemailer.createTransport('smtps://'+ smtp.user +':'+ smtp.password +'@' + smtp.host);
+
+	var mailOptions = {
+	    from: 'Ping Service <daniel.ruuth@gmail.com>',
+	    to: 'unset', // list of receivers
+	    subject: 'Pingservice', // Subject line
+	    text: '' // plaintext body
+	};
 }
 
-var mailOptions = {
-    from: 'Ping Service <daniel.ruuth@gmail.com>',
-    to: 'unset', // list of receivers
-    subject: 'Pingservice', // Subject line
-    text: '' // plaintext body
-};
 
-var saveStatusToDB = function( where, data ){
-	db.collection( 'pingback_status' , function(err, collection) {
-        collection.updateOne(where, 
-        	{
-        		$set: data,
-        		$currentDate: { "lastModified": true }
-      		}, 
-        	{
-        		upsert:true, 
-        		safe:true
-        	}, 
-        	function(err, result) {
-            	if (err) {
-                	console.log({'error':'An error has occurred'});
-                	console.log(err);
-            	}
-        	}
-        );
-    });
-}
 
 
 var checkStatus = function(){
-	var currentStatus, sendMail;
+	var currentStatus, sendMail, message;
 
 	hosts.forEach(function(host){
 	    ping.sys.probe(host.host, function(isAlive){
@@ -96,60 +73,42 @@ var checkStatus = function(){
 	        //console.log(msg);
 
 	        currentStatus = isAlive;
-	        mailOptions.to = host.contact;
+	        
 	        statusChanged = false;
 
 	        if( currentStatus != host.status && isAlive === false ){
-	        	console.log( 'Host ' + host.host + ' is down.' );
-
-	        	
-	        	mailOptions.text = 'Host ' + host.host + ' is down.';
+	        	message = 'Host ' + host.host + ' is down.';
 	        	statusChanged = true;
-
-	        	//Send mail to say service is down
 	        }else if( currentStatus != host.status && isAlive === true ){
-	        	console.log('Host ' + host.host + ' is back up.');
-
-	        	//Send mail to say service is back up
-	        	mailOptions.text = 'Host ' + host.host + ' is back up.';
-	        	statusChanged = false;
-	        	
+	        	message = 'Host ' + host.host + ' is back up.';
 	        }
 
 	        host.status = isAlive;
 
 	        if(statusChanged){
-	        	saveStatusToDB( {"host": host.host}, {"status": host.status, "incident_start": new Date().getTime() } )
+	        	incident_time = new Date().getTime();
+	        	io.emit('statusChanged', { host: host.host, status: host.status, incident_start: incident_time });
+	        	host.incident_start = incident_time;
 	        	
 	        	if(transporter){
+	        		mailOptions.text = message;
+	        		mailOptions.to = host.contact;
+
 		        	transporter.sendMail(mailOptions, function(error, info){
 					    if(error){
-					        return console.log(error);
+					    	console.log(error);
+					        return;
 					    }
 					});
-		        }else{
-		        	console.log( mailOptions.text  );
 		        }
 	        }
 
 	    });
 	});
+	lastupdated = new Date();
 
 }
 
-//setInterval( checkStatus, 5000 );
-console.log( 'Pingservice is running on ' + hosts.length + ' hosts' )
+setInterval( checkStatus, 60000 );
+console.log( 'Pingservice is running on ' + hosts.length + ' hosts' );
 
-
-//General collections
-app.set('view engine', 'jade');
-app.use(express.static('static'));
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Are we live?', hosts: hosts});
-});
-
-
-
-
-
-app.listen(3000);
---
+++ static/script.js
@@ -0,0 +1,80 @@
+"use strict"
+
+
+var socket = io.connect('http://localhost:3000');
+var $host_table = $('#host-table');
+
+function timeSince(timeStamp) {
+	
+    var now = new Date(),
+      secondsPast = (now.getTime() - timeStamp) / 1000;
+    if(secondsPast < 60){
+      return parseInt(secondsPast) + 's';
+    }
+    if(secondsPast < 3600){
+      return parseInt(secondsPast/60) + 'm';
+    }
+    if(secondsPast <= 86400){
+      return parseInt(secondsPast/3600) + 'h';
+    }
+    if(secondsPast > 86400){
+    	timestamp = new Date().setTime(timestamp);
+        day = timeStamp.getDate();
+        month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ","");
+        year = timeStamp.getFullYear() == now.getFullYear() ? "" :  " "+timeStamp.getFullYear();
+        return day + " " + month + year;
+    }
+  }
+
+socket.on('statusChanged', function (data) {
+    console.log(data);
+    //socket.emit('my other event', { my: 'data' });
+    var $row = $('#row' + data.id);
+
+    var $status = (data.status) ? $('<span class="glyphicon glyphicon-ok-circle" aria-hidden="true" />') : $('<span class="glyphicon glyphicon-remove-circle" aria-hidden="true" />');
+    if(data.status){
+    	$row.removeClass('error');
+    }else{
+    	$row.addClass('error');
+    }
+
+
+    $('td:eq(1)', $row).empty();
+    $('td:eq(1)', $row).append( $status );
+    $('td:eq(2)', $row).text( timeSince(data.incident_start) );
+
+});
+
+socket.on('data', function (data) {
+
+    var $row, $hostcell, $statuscell, $timecell;
+    $.each(data.hosts, function(index,item){
+    	console.log(item);
+    	$row = $('<tr />');
+    	$row.attr('id', 'row' + item.id);
+
+    	$hostcell = $('<td />');
+    	$statuscell = $('<td />');
+    	$timecell = $('<td />');
+
+    	$hostcell.text( item.host );
+    	
+
+    	var $status = (item.status) ? $('<span class="glyphicon glyphicon-ok-circle" aria-hidden="true" />') : $('<span class="glyphicon glyphicon-remove-circle" aria-hidden="true" />');
+
+ 		$status.appendTo($statuscell);
+
+    	$timecell.text( (item.incident_start) ? timeSince(item.incident_start) : '---');
+
+    	$hostcell.appendTo($row);
+    	$statuscell.appendTo($row);
+    	$timecell.appendTo($row);
+    	$row.appendTo($host_table);
+
+    	if(item.status){
+	    	$row.removeClass('error');
+	    }else{
+	    	$row.addClass('error');
+	    }
+    })
+});
